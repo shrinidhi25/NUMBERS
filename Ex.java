ec38f4454febab3e179c2d9ce56c67ee30084183
shrinidhi25 <shrinidhi.vijayan25@gmail.com>
2016-07-16 15:17:16 +0530

    Add files via upload

---
+++ Ex.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Ex {
+
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] b=new int[9];
+        for(int i=0;i<9;i++)
+        {
+            b[i]=sc.nextInt();
+        }
+        for(int i=0;i<b.length;i++)
+        {
+            for(int j=i+1;j<b.length;j++)
+            {
+                if(b[i]==b[j])
+                {
+                    b[i]=-1;
+                    b[j]=-1;
+                }       
+            }
+        }
+       for(int i=0;i<b.length;i++)
+       {
+         if(b[i]!=-1)
+         {
+             System.out.println(b[i]);
+         }
+       }
+    }
+}

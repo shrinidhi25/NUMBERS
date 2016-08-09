0a4d644086fd5a4690f346cf50c3a825bba317f7
shrinidhi25 <shrinidhi.vijayan25@gmail.com>
2016-08-09 15:55:40 +0530

    Add files via upload

---
+++ swappingtemp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10,b=20;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+     
+    }
+}

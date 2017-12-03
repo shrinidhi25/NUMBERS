bbd37dc8412b50669a4dc07a33f395186ba34150
shrinidhi25 <shrinidhi.vijayan25@gmail.com>
2017-12-03 19:18:08 +0530

    Add files via upload

---
+++ evenrodd.java
@@ -0,0 +1,22 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class evenrodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc= new Scanner(System.in);
+int num=sc.nextInt();
+if(num%2==0)
+{
+	System.out.println(num +"is even number");
+}
+else 
+{
+	System.out.println(num+"is odd");
+}
+
+	}
+
+}
